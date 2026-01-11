0b3813d88c26aee05d1395a7db4dd19661c71cf8
Detrut <detrut@inbox.ru>
2026-01-12 02:35:51 +0300

    Первый пуш проекта

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,10 +1,19 @@
 import { FC } from 'react';
 import { Preloader } from '../ui/preloader';
 import { IngredientDetailsUI } from '../ui/ingredient-details';
+import { Params, useParams } from 'react-router-dom';
+import { useSelector } from '../../services/store';
+import { getIngredientState } from '../../services/slices/ingredientsSlice';
 
 export const IngredientDetails: FC = () => {
-  /** TODO: взять переменную из стора */
-  const ingredientData = null;
+  const { list } = useSelector(getIngredientState);
+  const { id } = useParams<Params>();
+
+  const ingredientData = list.find((i) => {
+    if (i._id === id) {
+      return i;
+    }
+  });
 
   if (!ingredientData) {
     return <Preloader />;
--- src/pages/profile-orders/profile-orders.tsx
+++ src/pages/profile-orders/profile-orders.tsx
@@ -1,10 +1,22 @@
 import { ProfileOrdersUI } from '@ui-pages';
-import { TOrder } from '@utils-types';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { useSelector, useDispatch } from '../../services/store';
+import { getOrdersAll, getUserState } from '../../services/slices/userSlice';
+import { fetchFeeds } from '../../services/slices/feedSlice';
+import { Preloader } from '@ui';
 
 export const ProfileOrders: FC = () => {
-  /** TODO: взять переменную из стора */
-  const orders: TOrder[] = [];
+  const { userOrders, request } = useSelector(getUserState);
+  const dispatch = useDispatch();
 
-  return <ProfileOrdersUI orders={orders} />;
+  useEffect(() => {
+    dispatch(getOrdersAll());
+    dispatch(fetchFeeds());
+  }, []);
+
+  if (request === true) {
+    return <Preloader />;
+  }
+
+  return <ProfileOrdersUI orders={userOrders} />;
 };
