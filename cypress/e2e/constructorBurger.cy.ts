8f0344121500354ab6a96c158dd9391b54db9229
Detrut <detrut@inbox.ru>
2026-01-16 21:51:11 +0300

    Повтореные селекторы вынесены в константы для тестов cypress

--- cypress/e2e/constructorBurger.cy.ts
+++ cypress/e2e/constructorBurger.cy.ts
@@ -6,6 +6,11 @@ const MODAL_TITLE = '#modals h3';
 const MODAL_CLOSE = '#modals button';
 const MODAL_OVERLAY = '#modals > div:last-child';
 
+const INGREDIENT_BUN_NAME = 'Краторная булка N-200i';
+const SELECT_FILLING_TEXT = 'Выберите начинку';
+const INGREDIENT_LIST_ITEM = 'li';
+const ADD_BUTTON_TEXT = 'Добавить';
+
 describe('Страница конструктора бургера', () => {
   beforeEach(() => {
     cy.intercept('GET', '**/ingredients', { fixture: 'ingredients.json' }).as(
@@ -17,13 +22,13 @@ describe('Страница конструктора бургера', () => {
     cy.visit('/');
     cy.wait('@getIngredients');
 
-    cy.contains('Краторная булка N-200i')
-      .closest('li')
-      .contains('Добавить')
+    cy.contains(INGREDIENT_BUN_NAME)
+      .closest(INGREDIENT_LIST_ITEM)
+      .contains(ADD_BUTTON_TEXT)
       .click();
 
-    cy.contains('Краторная булка N-200i (верх)').should('exist');
-    cy.contains('Краторная булка N-200i (низ)').should('exist');
+    cy.contains(`${INGREDIENT_BUN_NAME} (верх)`).should('exist');
+    cy.contains(`${INGREDIENT_BUN_NAME} (низ)`).should('exist');
 
     cy.contains('Выберите булки').should('not.exist');
   });
@@ -31,16 +36,16 @@ describe('Страница конструктора бургера', () => {
   it('добавить начинку в конструктор', () => {
     cy.visit('/');
     cy.wait('@getIngredients');
-    cy.contains('Выберите начинку').should('exist');
-    cy.contains('Соус Spicy-X').closest('li').contains('Добавить').click();
-    cy.contains('Выберите начинку').should('not.exist');
+    cy.contains(SELECT_FILLING_TEXT).should('exist');
+    cy.contains('Соус Spicy-X').closest(INGREDIENT_LIST_ITEM).contains(ADD_BUTTON_TEXT).click();
+    cy.contains(SELECT_FILLING_TEXT).should('not.exist');
   });
 
   describe('модальное окно ингредиента', () => {
     it('открыть и показать свойства ингредиента', () => {
       cy.visit('/');
       cy.wait('@getIngredients');
-      cy.contains('Краторная булка N-200i').click();
+      cy.contains(INGREDIENT_BUN_NAME).click();
       cy.get(MODAL_CONTAINER).should('not.be.empty');
       cy.get(MODAL).should('be.visible');
       cy.get(MODAL_TITLE)
@@ -48,7 +53,7 @@ describe('Страница конструктора бургера', () => {
         .and('contain.text', 'Детали ингредиента');
 
       cy.get(MODAL).within(() => {
-        cy.contains('Краторная булка N-200i').should('be.visible');
+        cy.contains(INGREDIENT_BUN_NAME).should('be.visible');
         cy.contains('Калории, ккал').should('be.visible');
       });
     });
@@ -57,7 +62,7 @@ describe('Страница конструктора бургера', () => {
       cy.visit('/');
       cy.wait('@getIngredients');
 
-      cy.contains('Краторная булка N-200i').click();
+      cy.contains(INGREDIENT_BUN_NAME).click();
       cy.get(MODAL_CONTAINER).should('not.be.empty');
       cy.get(MODAL).should('be.visible');
       cy.get(MODAL_CLOSE).click();
@@ -67,7 +72,7 @@ describe('Страница конструктора бургера', () => {
     it('закрывается по оверлею', () => {
       cy.visit('/');
       cy.wait('@getIngredients');
-      cy.contains('Краторная булка N-200i').click();
+      cy.contains(INGREDIENT_BUN_NAME).click();
       cy.get(MODAL_CONTAINER).should('not.be.empty');
       cy.get(MODAL).should('be.visible');
       cy.get(MODAL_OVERLAY).click({ force: true });
@@ -77,7 +82,7 @@ describe('Страница конструктора бургера', () => {
     it('закрывается по Escape', () => {
       cy.visit('/');
       cy.wait('@getIngredients');
-      cy.contains('Краторная булка N-200i').click();
+      cy.contains(INGREDIENT_BUN_NAME).click();
       cy.get(MODAL_CONTAINER).should('not.be.empty');
       cy.get(MODAL).should('be.visible');
       cy.get('body').type('{esc}');
@@ -111,13 +116,13 @@ describe('Страница конструктора бургера', () => {
     });
 
     it('сборка бургера, оформление заказа', () => {
-      cy.contains('Краторная булка N-200i')
-        .closest('li')
-        .contains('Добавить')
+      cy.contains(INGREDIENT_BUN_NAME)
+        .closest(INGREDIENT_LIST_ITEM)
+        .contains(ADD_BUTTON_TEXT)
         .click();
 
-      cy.contains('Соус Spicy-X').closest('li').contains('Добавить').click();
-      cy.contains('Выберите начинку').should('not.exist');
+      cy.contains('Соус Spicy-X').closest(INGREDIENT_LIST_ITEM).contains(ADD_BUTTON_TEXT).click();
+      cy.contains(SELECT_FILLING_TEXT).should('not.exist');
       cy.contains('button', 'Оформить заказ').click();
 
       cy.wait('@createOrder');
@@ -132,7 +137,7 @@ describe('Страница конструктора бургера', () => {
       cy.contains('идентификатор заказа').should('not.exist');
 
       cy.contains('Выберите булки').should('be.visible');
-      cy.contains('Выберите начинку').should('be.visible');
+      cy.contains(SELECT_FILLING_TEXT).should('be.visible');
     });
   });
 });
